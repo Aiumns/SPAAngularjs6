ebc98aac0467effb8560e0c8b97e3dfd395b09fb
Aiumns <jitendramcs2013@yahoo.com>
2018-12-31 00:29:31 +0530

    Add files via upload

---
+++ Default4.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default4.aspx.cs" Inherits="Default4" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>  
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>       
+        <table>          
+            <tr>
+                <td>
+                    Photo/Image
+                </td>
+                <td>
+                    <asp:FileUpload ID="fuImage" runat="server" accept=".png,.PNG,.PDF,.pdf,.jpeg,.JPEG,.jpg,.JPG" ></asp:FileUpload>   
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+                </td>
+                <td>
+                      <asp:GridView ID="gridview1" runat="server">
+                           <Columns>
+                                <asp:TemplateField>
+                                            <ItemTemplate>
+                                                  <asp:LinkButton ID="pdffle"  runat="server" Text='<%#Eval("Title") %>' OnClick="pdffle_Click" ></asp:LinkButton><br/>
+                                            </ItemTemplate>     
+                                </asp:TemplateField>
+                           </Columns>
+                      </asp:GridView>
+                      <asp:Repeater Visible="false" ID="Repeater1" runat="server">   <%--OnItemCommand="Repeater1_ItemCommand"--%>
+                        <ItemTemplate>
+                            
+                            <asp:LinkButton ID="LinkButton1" Visible="false" runat ="server" Text='<%#Eval("Title") %>' CommandName="Redirect" CommandArgument='<%#Eval("Filelocation") %>'></asp:LinkButton><br/>
+                        </ItemTemplate>
+                    </asp:Repeater>                   
+                    <asp:Button ID="btnBack" Visible="false" runat="server" Text="View File" onclick="btnBack_Click" />                        
+                </td>
+            </tr>
+        </table>
+
+        <iframe runat="server" id="fred" style="border:1px solid #666CCC" visible="false" title="PDF in an i-Frame" frameborder="1" scrolling="auto" height="1100" width="850" ></iframe>
+
+<%--        <object data="http://www.jigzone.com/im/photo400300.gif" type="application/pdf">
+              <embed src="http://www.jigzone.com/im/photo400300.gif" type="application/pdf" />
+        </object>--%>
+
+<%--        <iframe id="iFrame" width="500"></iframe>
+        <input type="button" value="Load" onclick="setIframeSRC('iFrame', 'file://C:\\Users\\JITENDRA\\Desktop\\msdn.pdf');" />--%>
+    
+        <%-- <iframe src = "DisplayPage.aspx?file=air-waybill.jpg" height = "200px" width = "200px"></iframe>--%>
+         <iframe src = "DisplayPage.aspx?file=360Logo.JPG"  style=" display:none;border:1px solid #666CCC" visible="false" title="PDF in an i-Frame" frameborder="1" scrolling="auto" height="1100" width="850"></iframe>
+         <%--<object data="your_url_to_pdf" type="application/pdf">
+            <embed src="your_url_to_pdf" type="application/pdf" />
+         </object>--%>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default4.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+using System.Net;
+
+public partial class Default4 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if (!IsPostBack)
+        //{
+        //    Response.ContentType = "image/png";
+        //    Response.WriteFile(Server.MapPath("~/userfiles/" + Request.QueryString["file"]));
+        //    Response.Flush();
+        //    Response.End();
+        //}
+        LoadData();
+       
+    }
+
+    protected void Repeater1_ItemCommand(object source, RepeaterCommandEventArgs e)
+    {
+        if (e.CommandName == "Redirect")
+        {
+            Response.Redirect(e.CommandArgument.ToString());
+        }
+
+    }
+
+    void LoadData()
+    {
+        string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        SqlConnection con = new SqlConnection(constr);
+        SqlDataAdapter ad = new SqlDataAdapter("select * from tblFileUpload", con);
+        DataSet ds = new DataSet();
+        ad.Fill(ds);
+        if(ds.Tables[0].Rows.Count>0)
+        {
+            gridview1.DataSource = ds;
+            gridview1.DataBind();
+        }       
+    }
+
+    protected void pdffle_Click(object sender, EventArgs e)
+    {
+        //LinkButton btn = (LinkButton)sender;
+        //GridViewRow row = (GridViewRow)btn.NamingContainer;
+        ////assuming you store the ID in a Hiddenield:
+        //LinkButton lnkbtn = (LinkButton)row.FindControl("HiddenID");
+        //int ID = int.Parse(lnkbtn.ID);
+
+        int rowindex = ((GridViewRow)((sender as Control)).NamingContainer).RowIndex;
+        string filelocation = gridview1.Rows[rowindex].Cells[3].Text;
+        string FilePath = Server.MapPath(filelocation);
+        WebClient user = new WebClient();
+
+        Byte[] Filebuffer = user.DownloadData(FilePath);
+
+        if(Filebuffer !=null)
+        {
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-length", Filebuffer.Length.ToString());
+            Response.BinaryWrite(Filebuffer);
+        }
+
+
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        string filename = Path.GetFileName(fuImage.PostedFile.FileName);
+        string filelocation = System.IO.Path.Combine("userfiles/",filename);
+        fuImage.SaveAs(Server.MapPath("~/userfiles/" + filename));
+        //pdffle.Attributes.Add("src", filelocation);
+        // fred.Attributes.Add("src", "http://www.jigzone.com/im/photo400300.gif");       
+
+        string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        SqlConnection con = new SqlConnection(constr);
+        con.Open();
+        SqlCommand cmd = new SqlCommand("Insert into tblFileUpload (Title,Filelocation) Values(@Title,@Filelocation)", con);
+        cmd.Parameters.AddWithValue("@Title", filename);
+        cmd.Parameters.AddWithValue("@Filelocation", filelocation);
+        cmd.ExecuteNonQuery();
+        cmd.Dispose();
+        con.Close();        
+    }
+    protected void btnBack_Click(object sender, EventArgs e)
+    {
+
+    }
+    
+}
